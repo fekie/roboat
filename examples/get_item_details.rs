df21fe9fec7cdc911992fdeaaa5c4a120e46918d
Chloe-Woahie <68732833+Chloe-Woahie@users.noreply.github.com>
2023-03-30 23:45:37 -0400

    chore: add item_details() example

--- examples/get_item_details.rs
+++ examples/get_item_details.rs
@@ -1,7 +1,20 @@
+use roboat::catalog::avatar_catalog::ItemParameters;
+use roboat::catalog::avatar_catalog::ItemType;
+use roboat::Client;
+
 #[tokio::main]
-async fn main() {
-    use roboat::Client;
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let item_1 = ItemParameters {
+        item_type: ItemType::Bundle,
+        id: 39,
+    };
 
     let client = Client::new();
-    let details = client.item_details().await;
+    let details = client.item_details(vec![item_1]).await?;
+    println!(
+        "Bundle Name: {} / Bundle Price: {}",
+        details[0].name, details[0].price
+    );
+
+    Ok(())
 }
