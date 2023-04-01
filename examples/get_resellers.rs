06a2e024dfb3319a51c2f5f3e07e5a0b869dcdc4
Chloe-Woahie <68732833+Chloe-Woahie@users.noreply.github.com>
2023-04-01 02:06:49 -0400

    chore: add example for resellers()

---
+++ examples/get_resellers.rs
@@ -0,0 +1,27 @@
+use clap::Parser;
+use roboat::Client;
+use roboat::Limit;
+
+#[derive(Parser, Debug)]
+struct Args {
+    #[arg(long, short)]
+    roblosecurity: String,
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let args = Args::parse();
+
+    let client = Client::new();
+    client.set_roblosecurity(args.roblosecurity);
+
+    let item_id = 1365767;
+    let limit = Limit::Ten;
+    let cursor = None;
+
+    let (resellers, _) = client.resellers(item_id, limit, cursor).await?;
+
+    println!("Lowest Price for Item {}: {}", item_id, resellers[0].price);
+
+    Ok(())
+}
