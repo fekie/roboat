5c27ba22521fc1125fb398a92dae0eea1409e97b
rdevhacking <118940147+rdevhacking@users.noreply.github.com>
2023-04-02 17:51:13 +0000

    work in progress

--- src/users/mod.rs
+++ src/users/mod.rs
@@ -1,7 +1,7 @@
 use serde::{Deserialize, Serialize};
 
 const USER_DETAILS_API: &str = "https://users.roblox.com/v1/users/authenticated";
-const USERS_SEARCH_API: &str = "https://users.roblox.com/v1/users/search"
+const USERS_SEARCH_API: &str = "https://users.roblox.com/v1/users/search";
 
 /// Basic information about the account of the Roblosecurity. Retrieved
 /// from <https://users.roblox.com/v1/users/authenticated>.
@@ -19,44 +19,10 @@ pub(crate) struct UserInformation {
     pub display_name: String,
 }
 
-#[allow(missing_docs)]
-#[derive(Clone, Debug, PartialEq, Eq, PartialOrd, Ord, Hash, Default, Serialize, Deserialize)]
-pub(crate) struct UserSearchUserInformation {
-    #[serde(rename(deserialize = "id"))]
-    #[serde(rename(deserialize = "user_id"))]
-    pub user_id: u64,
-    #[serde(rename(deserialize = "name"))]
-    #[serde(rename(deserialize = "username"))]
-    pub username: String,
-    #[serde(rename(deserialize = "hasVerifiedBadge"))]
-    #[serde(rename(deserialize = "has_verified_badge"))]
-    pub has_verified_badge: bool,
-    #[serde(rename(deserialize = "previousUsernames"))]
-    #[serde(rename(deserialize = "previous_usernames"))]
-    pub previous_usernames: Vec<String>,
-    #[serde(rename(deserialize = "displayName"))]
-    #[serde(rename(deserialize = "display_name"))]
-    pub display_name: String,
-}
-
-#[allow(missing_docs)]
-#[derive(Clone, Debug, PartialEq, Eq, PartialOrd, Ord, Hash, Default, Serialize, Deserialize)]
-pub (crate) struct UserSearch {
-    #[serde(rename(deserialize = "previousPageCursor"))]
-    #[serde(rename(deserialize = "previous_page_cursor"))]
-    pub previous_page_cursor: String,
-    #[serde(rename(deserialize = "nextPageCursor"))]
-    #[serde(rename(deserialize = "next_page_cursor"))]
-    pub next_page_cursor: String,
-    #[serde(rename(deserialize = "data"))]
-    #[serde(rename(deserialize = "data"))]
-    pub data: Vec<UserSearchUserInformation>,
-}
-
 mod internal {
     use super::{UserInformation, USER_DETAILS_API};
-    use super::{UserSearch, USERS_SEARCH_API};
-    use crate::{Client, RoboatError};
+    use super::{USERS_SEARCH_API};
+    use crate::{Client, Limit, RoboatError};
     use reqwest::header;
 
     impl Client {
@@ -91,24 +57,26 @@ mod internal {
         }
 
         pub(crate) async fn users_search_internal(
+            keyword: &str,
+            limit: Limit,
             &self,
-        ) -> Result<UserSearch, RoboatError> {
+        ) -> Result<reqwest_types::UserSearchResponse, RoboatError> {
+
+
+        let formatted_url = format!(
+            "{}?keyword={}&limit={}",
+            USERS_SEARCH_API,  keyword, limit
+        );
 
             //todo pass parameters
             let request_result = self
                 .reqwest_client
-                .get(USERS_SEARCH_API)
+                .get(formatted_url)
                 .send()
                 .await;
 
             let response = Self::validate_request_result(request_result).await?;
-            let user_search = Self::parse_to_raw::<UserSearch>(response).await?;
-
-            // Cache results.
-            *self.previous_page_cursor.lock().unwrap() = Some(user_search.previous_page_cursor.clone());
-            *self.next_page_cursor.lock().unwrap() = Some(user_search.next_page_cursor.clone());
-            *self.data.lock().unwrap() = Some(user_search.data.clone());
-
+            let user_search = Self::parse_to_raw::<reqwest_types::UserSearchResponse>(response).await?;
             Ok(user_search)
         }
     }
---
+++ src/users/reqwest_types.rs
@@ -0,0 +1,28 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize)]
+pub(super) struct UserSearchUserInformationRaw {
+    #[serde(rename(deserialize = "id"))]
+    pub user_id: u64,
+    #[serde(rename(deserialize = "name"))]
+    pub username: String,
+    #[serde(rename(deserialize = "hasVerifiedBadge"))]
+    pub has_verified_badge: bool,
+    #[serde(rename(deserialize = "previousUsernames"))]
+    pub previous_usernames: Vec<String>,
+    #[serde(rename(deserialize = "displayName"))]
+    pub display_name: String,
+}
+
+#[derive(Serialize, Deserialize)]
+pub (super) struct UserSearchResponse {
+    #[serde(rename(deserialize = "previousPageCursor"))]
+    #[serde(rename(deserialize = "previous_page_cursor"))]
+    pub previous_page_cursor: String,
+    #[serde(rename(deserialize = "nextPageCursor"))]
+    #[serde(rename(deserialize = "next_page_cursor"))]
+    pub next_page_cursor: String,
+    #[serde(rename(deserialize = "data"))]
+    #[serde(rename(deserialize = "data"))]
+    pub data: Vec<UserSearchUserInformation>,
+}
