70f5c6f9341a68e94a503d672898637b1bdd6e87
Chloe-Woahie <68732833+Chloe-Woahie@users.noreply.github.com>
2023-04-02 02:10:42 -0400

    chore: add example for register_presence()

---
+++ examples/register_presence.rs
@@ -0,0 +1,24 @@
+use clap::Parser;
+use roboat::Client;
+
+#[derive(Parser, Debug)]
+struct Args {
+    #[arg(long, short)]
+    roblosecurity: String,
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let args = Args::parse();
+    let client = Client::new();
+    client.set_roblosecurity(args.roblosecurity);
+
+    let result = client.register_presence().await;
+
+    match result {
+        Ok(()) => println!("Registered presence!"),
+        Err(e) => println!("Failed to register presence. Reason: {}", e),
+    }
+
+    Ok(())
+}
