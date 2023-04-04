16bb8955a0ae4072dffb421e0a96b0678f726913
Chloe-Woahie <68732833+Chloe-Woahie@users.noreply.github.com>
2023-04-04 16:19:34 -0400

    style: misc formatting changes, added todos

--- src/users/mod.rs
+++ src/users/mod.rs
@@ -1,4 +1,8 @@
+use crate::{Client, Limit, RoboatError};
+use reqwest::header;
 use serde::{Deserialize, Serialize};
+
+mod reqwest_types;
 
 const USER_DETAILS_API: &str = "https://users.roblox.com/v1/users/authenticated";
 const USERS_SEARCH_API: &str = "https://users.roblox.com/v1/users/search";
@@ -37,90 +41,79 @@ pub struct UserList {
     pub data: Vec<User>,
 }
 
-mod internal {
-    use super::USERS_SEARCH_API;
-    use super::{User, UserInformation, UserList, USER_DETAILS_API};
-    use crate::{Client, Limit, RoboatError};
-    use reqwest::header;
+impl Client {
+    /// Grabs information about the user from <https://catalog.roblox.com/v1/catalog/items/details> using the
+    /// Roblosecurity inside the client.
+    ///
+    /// This is only for internal use. Use [`Client::user_id`], [`Client::username`], and [`Client::display_name`] instead.
+    ///
+    /// # Notes
+    /// * Requires a valid roblosecurity.
+    pub(crate) async fn user_information_internal(&self) -> Result<UserInformation, RoboatError> {
+        let cookie = self.create_cookie_string()?;
 
-    mod reqwest_types;
+        let request_result = self
+            .reqwest_client
+            .get(USER_DETAILS_API)
+            .header(header::COOKIE, cookie)
+            .send()
+            .await;
 
-    impl Client {
-        /// Grabs information about the user from <https://catalog.roblox.com/v1/catalog/items/details> using the
-        /// Roblosecurity inside the client.
-        ///
-        /// This is only for internal use. Use [`Client::user_id`], [`Client::username`], and [`Client::display_name`] instead.
-        ///
-        /// # Notes
-        /// * Requires a valid roblosecurity.
-        pub(crate) async fn user_information_internal(
-            &self,
-        ) -> Result<UserInformation, RoboatError> {
-            let cookie = self.create_cookie_string()?;
+        let response = Self::validate_request_result(request_result).await?;
+        let user_information = Self::parse_to_raw::<UserInformation>(response).await?;
 
-            let request_result = self
-                .reqwest_client
-                .get(USER_DETAILS_API)
-                .header(header::COOKIE, cookie)
-                .send()
-                .await;
+        // Cache results.
+        *self.user_id.lock().unwrap() = Some(user_information.user_id as u64);
+        *self.username.lock().unwrap() = Some(user_information.username.clone());
+        *self.display_name.lock().unwrap() = Some(user_information.display_name.clone());
 
-            let response = Self::validate_request_result(request_result).await?;
-            let user_information = Self::parse_to_raw::<UserInformation>(response).await?;
+        Ok(user_information)
+    }
 
-            // Cache results.
-            *self.user_id.lock().unwrap() = Some(user_information.user_id as u64);
-            *self.username.lock().unwrap() = Some(user_information.username.clone());
-            *self.display_name.lock().unwrap() = Some(user_information.display_name.clone());
+    // todo: make external example
+    // todo: make it use roblosecurity if available
+    // todo: write docs with doc example
+    // todo: note the previous todos are for this one shark guy and should be resolved within a couple of days (or ill handle it)
+    pub async fn user_search(
+        &self,
+        keyword: String,
+        limit: Limit,
+        cursor: Option<String>,
+    ) -> Result<UserList, RoboatError> {
+        let limit = limit.to_u64();
+        let cursor = cursor.unwrap_or_default();
 
-            Ok(user_information)
-        }
+        let formatted_url = format!(
+            "{}?keyword={}&limit={}&cursor={}",
+            USERS_SEARCH_API, keyword, limit, cursor
+        );
 
-        // todo: make external example
-        // todo: make it use roblosecurity if available
-        // todo: write docs with doc example
-        // todo: note the previous todos are for this one shark guy and should be resolved within a couple of days (or ill handle it)
-        pub async fn users_search(
-            &self,
-            keyword: String,
-            limit: Limit,
-            cursor: Option<String>,
-        ) -> Result<UserList, RoboatError> {
-            let limit = limit.to_u64();
-            let cursor = cursor.unwrap_or_default();
+        let request_result = self.reqwest_client.get(formatted_url).send().await;
 
-            let formatted_url = format!(
-                "{}?keyword={}&limit={}&cursor={}",
-                USERS_SEARCH_API, keyword, limit, cursor
-            );
+        let response = Self::validate_request_result(request_result).await?;
 
-            let request_result = self.reqwest_client.get(formatted_url).send().await;
+        let raw = Self::parse_to_raw::<reqwest_types::UserSearchResponse>(response).await?;
 
-            let response = Self::validate_request_result(request_result).await?;
+        let mut users = Vec::new();
 
-            let raw = Self::parse_to_raw::<reqwest_types::UserSearchResponse>(response).await?;
-
-            let mut users = Vec::new();
-
-            for user in raw.data {
-                let user_data = User {
-                    user_id: user.user_id,
-                    username: user.username,
-                    display_name: user.display_name,
-                    has_verified_badge: user.has_verified_badge,
-                    previous_usernames: user.previous_usernames,
-                };
-
-                users.push(user_data);
-            }
-
-            let result = UserList {
-                previous_page_cursor: raw.previous_page_cursor,
-                next_page_cursor: raw.next_page_cursor,
-                data: users,
+        for user in raw.data {
+            let user_data = User {
+                user_id: user.user_id,
+                username: user.username,
+                display_name: user.display_name,
+                has_verified_badge: user.has_verified_badge,
+                previous_usernames: user.previous_usernames,
             };
 
-            Ok(result)
+            users.push(user_data);
         }
+
+        let result = UserList {
+            previous_page_cursor: raw.previous_page_cursor,
+            next_page_cursor: raw.next_page_cursor,
+            data: users,
+        };
+
+        Ok(result)
     }
 }
