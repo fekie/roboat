22fc9f6ece14d3a02a64b9d45e3e5b5d450d0735
Chloe-Woahie <68732833+Chloe-Woahie@users.noreply.github.com>
2023-05-11 12:57:23 -0400

    chore: add example for avatar_catalog_search

--- examples/avatar_catalog_search.rs
+++ examples/avatar_catalog_search.rs
@@ -1,16 +1,42 @@
-use roboat::catalog::avatar_catalog::search_query::{AvatarSearchQuery, AvatarSearchQueryBuilder};
-use roboat::catalog::avatar_catalog::ItemArgs;
-use roboat::ClientBuilder;
+use roboat::catalog::avatar_catalog::search_query::AvatarSearchQueryBuilder;
+use roboat::catalog::avatar_catalog::Category;
+use roboat::{ClientBuilder, RoboatError};
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let query = AvatarSearchQueryBuilder::new()
         .keyword("cute".to_owned())
+        .category(Category::Accessories)
         .build();
 
     let client = ClientBuilder::new().build();
 
-    let foo = client.avatar_catalog_search(query).await?;
+    let mut next_cursor = None;
+
+    loop {
+        let (items, cursor) = match client
+            .avatar_catalog_search(&query, next_cursor.clone())
+            .await
+        {
+            Ok(x) => x,
+            Err(e) => match e {
+                RoboatError::TooManyRequests => {
+                    println!("Too many requests, waiting 60 seconds...");
+                    std::thread::sleep(std::time::Duration::from_secs(60));
+                    continue;
+                }
+                _ => return Err(e.into()),
+            },
+        };
+
+        println!("Items on this page: {}", items.len());
+
+        if cursor.is_none() || items.is_empty() {
+            break;
+        }
+
+        next_cursor = cursor;
+    }
 
     Ok(())
 }
