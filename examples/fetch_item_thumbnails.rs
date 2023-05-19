a3592c653da9e8a2c571b574b15b294337c412cc
Chloe-Woahie <68732833+Chloe-Woahie@users.noreply.github.com>
2023-05-19 15:52:52 -0400

    chore: add example for asset_thumbnail_url_bulk

---
+++ examples/fetch_item_thumbnails.rs
@@ -0,0 +1,20 @@
+use roboat::thumbnails::ThumbnailAssetSize;
+use roboat::ClientBuilder;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let asset_id_1 = 20418400;
+    let asset_id_2 = 12660007639;
+
+    let size = ThumbnailAssetSize::S420x420;
+
+    let client = ClientBuilder::new().build();
+    let urls = client
+        .asset_thumbnail_url_bulk(vec![asset_id_1, asset_id_2], size)
+        .await?;
+
+    println!("Asset {} thumbnail url: {}", asset_id_1, urls[0]);
+    println!("Asset {} thumbnail url: {}", asset_id_2, urls[1]);
+
+    Ok(())
+}
