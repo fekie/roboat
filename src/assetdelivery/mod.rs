954a45bab2465f576750785572f5ca15e718be7c
Brandon <badamsva@gmail.com>
2025-06-20 16:00:54 -0400

    feat: add asset_delivery v2 API and the /id method

--- src/assetdelivery/mod.rs
+++ src/assetdelivery/mod.rs
@@ -1,12 +1,34 @@
 // mod request_types;
 
 const ASSETDELIVERY_ASSET_API: &str = "https://assetdelivery.roblox.com/v1/asset/?ID={id}";
+const ASSETDELIVERY_V2_API: &str = "https://assetdelivery.roblox.com/v2";
 
-use crate::{Client, RoboatError, XCSRF_HEADER};
+use crate::{assetdelivery::request_types::AssetMetaData, Client, RoboatError, XCSRF_HEADER};
 use bytes::Bytes;
 use reqwest::header;
 
+mod request_types;
+
 impl Client {
+    /// Gets Meta data from item, this works on Animations, outfits, places and other assets.
+    /// This will return <AssetMetaData> which has asset_type_id and the download for the file.
+    /// Also more information like if the item is deleted.
+    /// #Notes
+    /// Requires valid roblosecurity
+    pub async fn fetch_asset_metadata(&self, asset_id: u64) -> Result<AssetMetaData, RoboatError> {
+        match self.fetch_asset_metadata_internal(asset_id).await {
+            Ok(x) => Ok(x),
+            Err(e) => match e {
+                RoboatError::InvalidXcsrf(new_xcsrf) => {
+                    self.set_xcsrf(new_xcsrf).await;
+
+                    self.fetch_asset_metadata_internal(asset_id).await
+                }
+                _ => Err(e),
+            },
+        }
+    }
+
     /// Downloads a raw asset bytes using the endpoint <https://assetdelivery.roblox.com/v1/asset/?id={id}>.
     ///
     /// # Notes
@@ -38,6 +60,8 @@ impl Client {
     /// # Ok(())
     /// # }
     /// ```
+    // WARNING: Theres a V2 API https://assetdelivery.roblox.com/v2/assetid/119472671657225 that
+    // has location of the file. Migrate to it if they ever ratelimit/remove the v1 API
     pub async fn fetch_asset_data(&self, asset_id: u64) -> Result<Bytes, RoboatError> {
         let cookie_string = self.cookie_string()?;
         let formatted_url = ASSETDELIVERY_ASSET_API.replace("{id}", &asset_id.to_string());
@@ -56,5 +80,35 @@ impl Client {
 
         let bytes = response.bytes().await.map_err(RoboatError::ReqwestError)?;
         Ok(bytes)
+    }
+}
+
+mod internal {
+    use super::request_types;
+    use crate::assetdelivery::{AssetMetaData, ASSETDELIVERY_V2_API};
+    use crate::{Client, RoboatError};
+    use reqwest::header;
+
+    impl Client {
+        pub(super) async fn fetch_asset_metadata_internal(
+            &self,
+            asset_id: u64,
+        ) -> Result<AssetMetaData, RoboatError> {
+            let cookie = self.cookie_string()?;
+            // let xcsrf = self.xcsrf().await;
+            let formatted_url = format!("{}/asset/{}", ASSETDELIVERY_V2_API, asset_id);
+
+            let request_result = self
+                .reqwest_client
+                .get(formatted_url)
+                .header(header::COOKIE, cookie)
+                .send()
+                .await;
+
+            let response = Self::validate_request_result(request_result).await?;
+            let meta_data = Self::parse_to_raw::<request_types::AssetMetaData>(response).await?;
+
+            Ok(meta_data)
+        }
     }
 }
---
+++ src/assetdelivery/request_types.rs
@@ -0,0 +1,30 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct AssetMetaData {
+    pub locations: Vec<AssetLocation>,
+    pub request_id: String,
+    #[serde(rename = "IsHashDynamic")]
+    pub is_hash_dynamic: bool,
+    #[serde(rename = "IsCopyrightProtected")]
+    pub is_copyright_protected: bool,
+    pub is_archived: bool,
+    pub asset_type_id: u32,
+    pub is_recordable: bool,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct AssetLocation {
+    pub asset_format: String,
+    pub location: String,
+    pub asset_metadatas: Vec<AssetMetadata>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct AssetMetadata {
+    pub metadata_type: u32,
+    pub value: String,
+}
