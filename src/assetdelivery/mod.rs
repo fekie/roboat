d60127ac7fd386b9d0d81863283302227635750e
Brandon <badamsva@gmail.com>
2025-06-22 02:24:43 -0400

    feat: add asset_id batch API that gets information from multiple Ids
    Add error to AssetMeta Response Struct, and add error checking in both
    asset batch and asset meta data response.

--- src/assetdelivery/mod.rs
+++ src/assetdelivery/mod.rs
@@ -1,20 +1,30 @@
 // mod request_types;
+#![allow(missing_docs)]
 
 const ASSETDELIVERY_ASSET_API: &str = "https://assetdelivery.roblox.com/v1/asset/?ID={id}";
 const ASSETDELIVERY_V2_API: &str = "https://assetdelivery.roblox.com/v2";
 
-use crate::{assetdelivery::request_types::AssetMetaData, Client, RoboatError, XCSRF_HEADER};
+use crate::{
+    assetdelivery::request_types::{AssetBatchPayload, AssetBatchResponse, AssetMetaData},
+    Client, RoboatError, XCSRF_HEADER,
+};
 use bytes::Bytes;
 use reqwest::header;
 
-mod request_types;
+/// All the payload/response structs
+pub mod request_types;
 
+/// WARNING: Some AssetDelivery V2 API returns Errors even when its status code 200
 impl Client {
     /// Gets Meta data from item, this works on Animations, outfits, places and other assets.
     /// This will return <AssetMetaData> which has asset_type_id and the download for the file.
     /// Also more information like if the item is deleted.
-    /// #Notes
+    ///
+    /// # Notes
     /// Requires valid roblosecurity
+    /// *Can return a sucess but still have error codes in the response
+    /// Doesn't need xcrf, but will add one if it gets 401
+
     pub async fn fetch_asset_metadata(&self, asset_id: u64) -> Result<AssetMetaData, RoboatError> {
         match self.fetch_asset_metadata_internal(asset_id).await {
             Ok(x) => Ok(x),
@@ -23,6 +33,86 @@ impl Client {
                     self.set_xcsrf(new_xcsrf).await;
 
                     self.fetch_asset_metadata_internal(asset_id).await
+                }
+                _ => Err(e),
+            },
+        }
+    }
+
+    /// Sends a batch request to fetch metadata for multiple assets.
+    ///
+    /// This method accepts a vector of `AssetBatchPayload` items, sends them
+    /// to the Roblox AssetDelivery v2 batch API endpoint, and returns a vector
+    /// of `AssetBatchResponse` objects corresponding to each requested asset.
+    ///
+    /// # Parameters
+    /// - `asset_batch`: A vector of `AssetBatchPayload` structs representing
+    ///   the assets for which metadata is requested.
+    ///
+    /// # Notes
+    /// Needs Roblox Cookie but not CSRF
+    /// Can return a sucess but still have error codes in the response
+    ///     
+    /// # Returns
+    /// Returns a `Result` containing a vector of `AssetBatchResponse` on success,
+    /// or a `RoboatError` if the request fails or the response is malformed.
+    ///
+    /// # Behavior
+    /// - Automatically handles `InvalidXcsrf` errors by refreshing the X-CSRF token
+    ///   and retrying the request once.
+    /// - Requires a valid `.ROBLOSECURITY` cookie set in the `Client`.
+    ///
+    /// # Example
+    ///
+    /// ```no_run
+    /// use clap::Parser;
+    /// use roboat::assetdelivery::request_types::AssetBatchPayload;
+    ///
+    /// #[derive(Parser, Debug)]
+    /// struct Args {
+    ///     #[arg(long, short)]
+    ///     roblosecurity: String,
+    /// }
+    ///
+    /// use roboat::ClientBuilder;
+    /// #[tokio::main]
+    /// async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    ///     let args = Args::parse();
+    ///     let client = ClientBuilder::new()
+    ///         .roblosecurity(args.roblosecurity)
+    ///         .build();
+    ///     let payloads = vec![AssetBatchPayload {
+    ///         asset_id: Some("105277031944789".to_string()),
+    ///         request_id: Some("0".to_string()),
+    ///         ..Default::default()
+    ///     }];
+    ///     let responses = client.post_asset_metadata_batch(payloads).await?;
+    ///     for response in responses {
+    ///         println!("Response for request {:?}", response);
+    ///     }
+    ///     Ok(())
+    ///}
+    /// ```
+    ///
+    /// # Errors
+    /// This function will return errors including but not limited to:
+    /// - Network or HTTP errors from the reqwest client
+    /// - Invalid or expired X-CSRF tokens (which it will attempt to refresh automatically)
+    /// - Malformed responses from the API
+
+    pub async fn post_asset_metadata_batch(
+        &self,
+        asset_batch: Vec<AssetBatchPayload>,
+    ) -> Result<Vec<AssetBatchResponse>, RoboatError> {
+        match self
+            .post_asset_metadata_batch_internal(asset_batch.clone())
+            .await
+        {
+            Ok(x) => Ok(x),
+            Err(e) => match e {
+                RoboatError::InvalidXcsrf(new_xcsrf) => {
+                    self.set_xcsrf(new_xcsrf).await;
+                    self.post_asset_metadata_batch_internal(asset_batch).await
                 }
                 _ => Err(e),
             },
@@ -60,6 +150,7 @@ impl Client {
     /// # Ok(())
     /// # }
     /// ```
+
     // WARNING: Theres a V2 API https://assetdelivery.roblox.com/v2/assetid/119472671657225 that
     // has location of the file. Migrate to it if they ever ratelimit/remove the v1 API
     pub async fn fetch_asset_data(&self, asset_id: u64) -> Result<Bytes, RoboatError> {
@@ -85,11 +176,46 @@ impl Client {
 
 mod internal {
     use super::request_types;
+    use crate::assetdelivery::request_types::{AssetBatchPayload, AssetBatchResponse};
     use crate::assetdelivery::{AssetMetaData, ASSETDELIVERY_V2_API};
     use crate::{Client, RoboatError};
     use reqwest::header;
 
     impl Client {
+        pub(super) async fn post_asset_metadata_batch_internal(
+            &self,
+            asset_payload: Vec<AssetBatchPayload>,
+        ) -> Result<Vec<AssetBatchResponse>, RoboatError> {
+            let cookie = self.cookie_string()?;
+            let formatted_url = format!("{ASSETDELIVERY_V2_API}/assets/batch");
+
+            let request_result = self
+                .reqwest_client
+                .post(formatted_url)
+                .header(header::COOKIE, cookie)
+                .json(&asset_payload)
+                .send()
+                .await;
+
+            let response = Self::validate_request_result(request_result).await?;
+            let meta_data =
+                Self::parse_to_raw::<Vec<request_types::AssetBatchResponse>>(response).await?;
+
+            // Scan response for roblox errors, if its 401 just return Invalid Cookie (Can't be
+            // CSRF on this API)
+            for batch_resp in &meta_data {
+                if let Some(errors) = &batch_resp.errors {
+                    for error in errors {
+                        // 401 Error will be .ROBLOSECURITY. and not CSRF.
+                        if error.code == 401 {
+                            return Err(RoboatError::InvalidRoblosecurity);
+                        }
+                    }
+                }
+            }
+
+            Ok(meta_data)
+        }
         pub(super) async fn fetch_asset_metadata_internal(
             &self,
             asset_id: u64,
@@ -107,6 +233,21 @@ mod internal {
 
             let response = Self::validate_request_result(request_result).await?;
             let meta_data = Self::parse_to_raw::<request_types::AssetMetaData>(response).await?;
+
+            // Scan response for roblox errors, if its 401 just return Invalid Cookie (Can't be
+            // CSRF on this API)
+            if let Some(errors) = &meta_data.errors {
+                for error in errors {
+                    // 401 Error will be .ROBLOSECURITY. and not CSRF.
+                    if error.code == 401 {
+                        return Err(RoboatError::InvalidRoblosecurity);
+                    } else {
+                        // this API either gets one Error or Asset. Return the Err
+                        // NOTE: This Error could be that the Asset is Private.
+                        return Err(RoboatError::UnidentifiedStatusCode(error.code));
+                    }
+                }
+            }
 
             Ok(meta_data)
         }
--- src/assetdelivery/request_types.rs
+++ src/assetdelivery/request_types.rs
@@ -1,8 +1,15 @@
 use serde::{Deserialize, Serialize};
+use serde_with::skip_serializing_none;
 
+//NOTE: Asset Deliver API responses are really dynamic and can miss all fields, to show errors so
+//I use option to make Rust not panic because roblox responds with nonsense.
+
+//https://create.roblox.com/docs/cloud/legacy/assetdelivery/v2#/AssetFetchV2/get_v2_assetId__assetId_
+/// Structs For MetaData /v2/assetId/{assetId} Endpoint
 #[derive(Debug, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub struct AssetMetaData {
+    pub errors: Option<Vec<RobloxError>>,
     pub locations: Vec<AssetLocation>,
     pub request_id: String,
     #[serde(rename = "IsHashDynamic")]
@@ -28,3 +35,83 @@ pub struct AssetMetadata {
     pub metadata_type: u8,
     pub value: String,
 }
+
+/// Structs for /v2/assets/batch All of these are optional to define what Asset you want information from.
+//{https://create.roblox.com/docs/cloud/legacy/assetdelivery/v2#/BatchV2/post_v2_assets_batch}
+#[skip_serializing_none]
+#[derive(Debug, Serialize, Deserialize, Clone, Default)]
+#[serde(default)]
+#[serde(rename_all = "camelCase")]
+pub struct AssetBatchPayload {
+    pub asset_name: Option<String>,
+    pub asset_type: Option<String>,
+    pub client_insert: Option<bool>,
+    pub place_id: Option<String>,
+    pub request_id: Option<String>,
+    pub script_insert: Option<bool>,
+    pub server_place_id: Option<String>,
+    pub universe_id: Option<String>,
+    pub accept: Option<String>,
+    pub encoding: Option<String>,
+    pub hash: Option<String>,
+    pub user_asset_id: Option<String>,
+    pub asset_id: Option<String>,
+    pub version: Option<String>,
+    pub asset_version_id: Option<String>,
+    pub module_place_id: Option<String>,
+    pub asset_format: Option<String>,
+    #[serde(rename = "roblox-assetFormat")]
+    pub roblox_asset_format: Option<String>,
+    pub content_representation_priority_list: Option<String>,
+    pub do_not_fallback_to_baseline_representation: Option<bool>,
+}
+
+/// Response for AssetBatch
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct AssetBatchResponse {
+    pub errors: Option<Vec<RobloxError>>,
+
+    pub locations: Option<Vec<Location>>,
+    pub request_id: Option<String>,
+    pub is_hash_dynamic: Option<bool>,
+    pub is_copyright_protected: Option<bool>,
+    pub is_archived: Option<bool>,
+    pub asset_type_id: Option<u8>,
+    pub content_representation_specifier: Option<ContentRepresentationSpecifier>,
+    pub is_recordable: Option<bool>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Location {
+    pub asset_format: Option<String>,
+    pub location: Option<String>,
+    pub asset_metadatas: Option<Vec<AssetMetadata>>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct ContentRepresentationSpecifier {
+    pub format: Option<String>,
+    pub major_version: Option<String>,
+    pub fidelity: Option<String>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct RobloxError {
+    pub code: u16,
+    pub message: String,
+    pub custom_error_code: Option<i32>,
+}
+
+// #[derive(Debug, Serialize, Deserialize)]
+// pub struct AssetError {
+//     #[serde(rename = "Code")]
+//     pub code: Option<u32>,
+//     #[serde(rename = "Message")]
+//     pub message: Option<String>,
+//     #[serde(rename = "CustomErrorCode")]
+//     pub custom_error_code: Option<u32>,
+// }
